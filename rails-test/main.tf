886eef869e850e054c89597fa555d4ef0f403b55
daiki-murakami <muranokami1@gmail.com>
2021-03-30 23:48:58 +0900

    terraform init

---
+++ rails-test/main.tf
@@ -0,0 +1,112 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "vpc-for-task-manager"
+  }
+}
+
+resource "aws_subnet" "public_1a" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1a"
+
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "subnet-for-task-manager-1"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw-for-task-manager"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "route-table-for-task-manager"
+  }
+}
+
+resource "aws_route" "public" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public_1a" {
+  subnet_id = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "main" {
+  name = "task-manager-first-sg"
+  description = "task-manager first sg"
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_security_group_rule" "inbound_http" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
+}
+
+resource "aws_security_group_rule" "inbound_ssh" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
+}
+
+resource "aws_security_group_rule" "inbound_self" {
+  type = "ingress"
+  from_port = 3306
+  to_port = 3306
+  protocol = "tcp"
+  source_security_group_id = aws_security_group.main.id
+  security_group_id = aws_security_group.main.id
+}
+
+resource "aws_security_group_rule" "outbound_http" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
+}
+
+resource "aws_ami" "main" {
+  ena_support = true
+  name = "amzn2-ami-hvm-2.0.20201126.0-x86_64-gp2"
+  description = "Amazon Linux 2 AMI 2.0.20201126.0 x86_64 HVM gp2"
+  root_device_name = "/dev/xvda"
+  virtualization_type = "hvm"
+}
+
+resource "aws_instance" "main" {
+  ami = aws_ami.main.id
+  instance_type = "t2.nano"
+  key_name = "key-for-task-manager"
+  subnet_id = aws_subnet.public_1a.id
+  associate_public_ip_address = true
+  disable_api_termination = true
+  tags = {
+    Name = "ec2-for-task-manager"
+  }
+}
